4476f8253ba5642693012cb1bf8777bfdaa478e4
柴倉大靖 <bakurarex@wlan-napt-001.u-aizu.ac.jp>
2019-07-31 18:09:32 +0900

    Greetins added

--- Dicegame.java
+++ Dicegame.java
@@ -8,6 +8,12 @@ class DiceGame{
 	a++;
 	b++;
 	c = a+b;
+	System.out.println("What is your name?");
+	String name = scan.nextLine();
+	System.out.println("Hello, "+ name +"!");
+
+
     }
 }
+# s1260136
 # s1260136
