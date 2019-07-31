edeb0df33093c122ac5d17783e545f79cd5f09ad
柴倉大靖 <bakurarex@wlan-napt-001.u-aizu.ac.jp>
2019-07-31 18:24:12 +0900

    Won/Lost message added

--- Dicegame.java
+++ Dicegame.java
@@ -11,7 +11,22 @@ class DiceGame{
 	System.out.println("What is your name?");
 	String name = scan.nextLine();
 	System.out.println("Hello, "+ name +"!");
+	
+	System.out.println("Rolling the dice...");
 
+	System.out.println("Die 1: " + a);
+
+	System.out.println("Die 2: " + b);
+
+	System.out.println("Total value: " + c);
+	if(c>7){
+	    System.out.println(name + "won!");
+
+	}
+	else{
+
+	    	System.out.println(name +"lost!");
+	}
 
     }
 }
