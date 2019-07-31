4d4eba6681a7a4372ff907849faa9d17df856f08
柴倉大靖 <bakurarex@wlan-napt-001.u-aizu.ac.jp>
2019-07-31 18:01:01 +0900

    First revision

---
+++ Dicegame.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class DiceGame{
+    public static void main(string[]args){
+	Scanner scan = new Scanner(System.in);
+	Random rand = new Random();
+	int a = rand.nextInt(6);
+	int b = rand.nextInt(6);
+	a++;
+	b++;
+	c = a+b;
+    }
+}
+# s1260136
